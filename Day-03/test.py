d01a7cadee418ebc6f51ebcd15cebea64006b903
pranjul <pranjul2501@gmail.com>
2025-06-18 18:22:44 +0530

    practice

---
+++ Day-03/test.py
@@ -0,0 +1,3 @@
+ec2_instance = 'project-xyz-instance'
+
+print(ec2_instance)
---
+++ Day-04/calucator.py
@@ -0,0 +1,32 @@
+num1 = 15
+num2 = 16
+
+def addition():
+    add = num1 + num2
+
+    print(add)
+
+
+
+def sub():
+    subtration = num2 -num1 
+    print(subtration )
+    
+def mul():
+    mp = num1 * num2
+    print(mp)
+
+def division():
+    dv = num2 / num1 
+    print(dv)
+
+
+
+division()
+
+addition()
+
+sub()
+
+mul()
+
